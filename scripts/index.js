2cb420898f4d23cf0efb7e15064ffe0112873d8b
huanamigo <arturfurtak5@gmail.com>
2022-07-02 10:35:17 +0200

    initial

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+const mainQuote = document.querySelector("#mainQuote");
+const quoteID = document.querySelector("#quoteID");
+const randomButton = document.querySelector(".randomizeBtn");
+
+function getAdvice() {
+  console.log("chuj");
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      mainQuote.innerHTML = '"' + data.slip.advice + '"';
+      quoteID.innerHTML = "ADVICE #" + data.slip.id;
+    });
+}
+
+getAdvice();
+randomButton.addEventListener("click", getAdvice);
